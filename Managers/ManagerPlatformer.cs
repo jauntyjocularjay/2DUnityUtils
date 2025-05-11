44b12a2308d4cda1f06e2482aac50d597680cf7d
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-05-11 12:23:03 -0700

    Created the background manager and a sky class, adjustments to camera following

--- Managers/ManagerPlatformer.cs
+++ Managers/ManagerPlatformer.cs
@@ -22,44 +22,60 @@ public class PlatformerManager : Manager
     }
     void SetCameraPosition()
     {
-        
-        if( player.Transform().position.x + player.data.cameraOffset.x >= cameraMinimumPosition.x && player.Transform().position.x + player.data.cameraOffset.x <= cameraMaximumPosition.x )
+        if(player.Transform().position.x + player.data.cameraOffset.x <= cameraMinimumPosition.x)
         {
-            cameraTX.position = new Vector3(player.Transform().position.x + player.data.cameraOffset.x, cameraTX.position.y, cameraTX.position.z);
+            cameraTX.position = new Vector3
+            (
+                cameraMinimumPosition.x, 
+                cameraTX.position.y, 
+                cameraTX.position.z
+            );
         }
-        else if (player.Transform().position.x + player.data.cameraOffset.x <= cameraMinimumPosition.x)
+        else if(player.Transform().position.x + player.data.cameraOffset.x >= cameraMaximumPosition.x)
         {
-            cameraTX.position = new Vector3(cameraMinimumPosition.x, cameraTX.position.y, cameraTX.position.z);
+            cameraTX.position = new Vector3
+            (
+                cameraMaximumPosition.x, 
+                cameraTX.position.y, 
+                cameraTX.position.z
+            );
         }
-        else if (player.Transform().position.x + player.data.cameraOffset.x >= cameraMaximumPosition.x)
+        else
         {
-            cameraTX.position = new Vector3(cameraMaximumPosition.x, cameraTX.position.y, cameraTX.position.z);
+            cameraTX.position = new Vector3
+            (
+                player.Transform().position.x + player.data.cameraOffset.x, 
+                cameraTX.position.y, 
+                cameraTX.position.z
+            );
         }
-        
-        if( player.Transform().position.y >= cameraMinimumPosition.y && cameraTX.position.y <= cameraMaximumPosition.y )
+
+        if(player.Transform().position.y + player.data.cameraOffset.y <= cameraMinimumPosition.y)
         {
-            cameraTX.position = new Vector3(cameraTX.position.x, player.Transform().position.y + player.data.cameraOffset.y, cameraTX.position.z);
+            cameraTX.position = new Vector3
+            (
+                cameraTX.position.x, 
+                cameraMinimumPosition.y, 
+                cameraTX.position.z
+            );
         }
-        else if (player.Transform().position.y + player.data.cameraOffset.y >= cameraMinimumPosition.y)
+        else if (player.Transform().position.y + player.data.cameraOffset.y >= cameraMaximumPosition.y)
         {
-            cameraTX.position = new Vector3(cameraTX.position.x, cameraMinimumPosition.y, cameraTX.position.z);
+            cameraTX.position = new Vector3
+            (
+                cameraTX.position.x, 
+                cameraMaximumPosition.y, 
+                cameraTX.position.z
+            );
         }
-        else if (player.Transform().position.y + player.data.cameraOffset.y <= cameraMaximumPosition.y)
+        else
         {
-            cameraTX.position = new Vector3(cameraTX.position.x, cameraMaximumPosition.y, cameraTX.position.z);
-        }
-        
-        if( player.Transform().position.z >= cameraMinimumPosition.z && cameraTX.position.z <= cameraMaximumPosition.z )
-        {
-            cameraTX.position = new Vector3(cameraTX.position.x, cameraTX.position.y, cameraTX.position.z + player.data.cameraOffset.z);
-        }
-        else if (player.Transform().position.z >= cameraMinimumPosition.z)
-        {
-            cameraTX.position = new Vector3(cameraTX.position.x, cameraTX.position.y, cameraMinimumPosition.z);
-        }
-        else if (player.Transform().position.z <= cameraMaximumPosition.z)
-        {
-            cameraTX.position = new Vector3(cameraTX.position.x, cameraTX.position.y, cameraMaximumPosition.z);
+            cameraTX.position = new Vector3
+            (
+                cameraTX.position.x, 
+                player.Transform().position.y + player.data.cameraOffset.y,
+                cameraTX.position.z
+            );
         }
     }
 
