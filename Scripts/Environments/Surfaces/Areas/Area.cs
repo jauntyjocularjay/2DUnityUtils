c47e8548366fb6446b782ea21821bc9730e14af3
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-06-05 04:09:24 -0700

    updated area

--- Scripts/Environments/Surfaces/Areas/Area.cs
+++ Scripts/Environments/Surfaces/Areas/Area.cs
@@ -1,18 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 
 
 
 namespace DMBTools
 {
-    public abstract class Area : Surface
+    public abstract class Area : Prop
     {
-        public BoxCollider2D collidr;
+        TilemapCollider2D collidr;
+        Rigidbody2D rb;
 
         public new void Start()
         {
             base.Start();
-            
+
+            SetCompositeCollider2D();
+            SetRigidbody2D();
+
+        }
+
+        public TilemapCollider2D CompositeCollider2D()
+        {
+            return collidr;
+        }
+        void SetCompositeCollider2D()
+        {
+            collidr = GetComponent<TilemapCollider2D>();
+
+            collidr.isTrigger = true;
+        }
+        public Rigidbody2D Rigidbody2D()
+        /* @method Rigidbody2D() returns the Rigidbody2D component from the GameObject */
+        {
+            return rb;
+        }
+        public void SetRigidbody2D()
+        /* @method Rigidbody2D() returns the Rigidbody2D component from the GameObject */
+        {
+            Rigidbody2D().bodyType = RigidbodyType2D.Static;
+            Rigidbody2D().constraints = RigidbodyConstraints2D.FreezeRotation;
         }
     }
 }
