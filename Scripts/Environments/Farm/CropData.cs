a1c178f26c83d24f44d0ac8c9cec779fdb7d6ba9
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-06-08 04:19:32 -0700

    added cropdata and croptypes

---
+++ Scripts/Environments/Farm/CropData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace DMBTools
+{
+[CreateAssetMenu(fileName = "Crop Data", menuName = "HelsingFarm/Data/Crop", order = 0)]
+public class CropData : ScriptableObject
+{
+    public CropType type;
+    public int currentPhase = 0;
+    public List<Sprite> sprites;
+
+
+
+}
+}
---
+++ Scripts/Environments/Farm/CropTypes.cs
@@ -0,0 +1,24 @@
+﻿
+namespace DMBTools
+{
+    public enum CropType
+    {
+        undefined = 0,
+        Beet = 1,
+        Blueberry = 2,
+        CabbageGreen = 3,
+        CabbageRed = 4,
+        Carrot = 5,
+        Cauliflower = 6,
+        Chile = 7,
+        Corn = 8,
+        Eggplant = 9,
+        Lettuce = 10,
+        Potato = 11,
+        Pumpkin = 12,
+        Radish = 13,
+        Tomato = 14,
+        Watermelon = 15,
+        Wheat = 16,
+    }
+}
