d9f3cc8346781996f539c7e3fc8c4e99464be1d6
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-06-12 08:37:52 -0700

    created the state class that holds a name and a flag

---
+++ Scripts/Resources/State.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DMBTools;
+
+[System.Serializable]
+public class State
+{
+    public string key;
+    public bool value;
+
+    public State(string key, bool value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+}
