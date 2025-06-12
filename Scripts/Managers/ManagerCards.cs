8ad8b478d5ebf11877db7f4795fccd4da6c2ca15
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-06-12 05:38:35 -0700

    added exile card array

--- Scripts/Managers/ManagerCards.cs
+++ Scripts/Managers/ManagerCards.cs
@@ -6,6 +6,7 @@ namespace DMBTools
 {
     public abstract class CardManager: Manager
     {
+        public Card[] exile = new Card[]{};
         new void Start()
         {
             base.Start();
