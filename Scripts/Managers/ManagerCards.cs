b1c46464ce6184041e94c14397f95bab9f0d7808
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-07-21 16:11:57 -0700

    Moved exile out of the game-manager, set ListSet to DMBTools namespace

--- Scripts/Managers/ManagerCards.cs
+++ Scripts/Managers/ManagerCards.cs
@@ -6,7 +6,7 @@ namespace DMBTools
 {
     public abstract class CardManager: Manager
     {
-        public Card[] exile = new Card[]{};
+        
         new void Start()
         {
             base.Start();
--- Scripts/Resources/ListSet.cs
+++ Scripts/Resources/ListSet.cs
@@ -2,74 +2,78 @@ using System;
 using System.Collections.Generic;
 
 
-[System.Serializable]
-public class ListSet<T>
+
+namespace DMBTools
 {
-    List<T> list;
-    int Count => list.Count;
-    public ListSet() => list = new List<T>();
-    public ListSet(int capacity) => list = new List<T>(capacity);
-    public ListSet(IEnumerable<T> collection) => list.AddRange(collection);
-
-    /*** Enumerator ***/
-    public IEnumerator<T> GetEnumerator() => list.GetEnumerator();
-
-    /*** Indexer ***/
-    public T this[int i]
+    [System.Serializable]
+    public class ListSet<T>
     {
-        get => list[i];
-        set
-        {
-            if (!list.Contains(value)) list[i] = value;
-        }
-    }
+        List<T> list;
+        int Count => list.Count;
+        public ListSet() => list = new List<T>();
+        public ListSet(int capacity) => list = new List<T>(capacity);
+        public ListSet(IEnumerable<T> collection) => list.AddRange(collection);
 
-    public bool Add(T element)
-    {
-        if (list.Contains(element))
-            return false;
-        else
+        /*** Enumerator ***/
+        public IEnumerator<T> GetEnumerator() => list.GetEnumerator();
+
+        /*** Indexer ***/
+        public T this[int i]
         {
-            list.Add(element);
-            return true;
-        }
-    }
-    public void AddRange(IEnumerable<T> collection)
-    {
-        foreach (T item in collection)
-        {
-            if (!Add(item))
+            get => list[i];
+            set
             {
-                Console.WriteLine($"WARNING: {item} is a duplicate, use Set() to update values");
+                if (!list.Contains(value)) list[i] = value;
             }
         }
+
+        public bool Add(T element)
+        {
+            if (list.Contains(element))
+                return false;
+            else
+            {
+                list.Add(element);
+                return true;
+            }
+        }
+        public void AddRange(IEnumerable<T> collection)
+        {
+            foreach (T item in collection)
+            {
+                if (!Add(item))
+                {
+                    Console.WriteLine($"WARNING: {item} is a duplicate, use Set() to update values");
+                }
+            }
+        }
+        public void Insert(int index, T element)
+        {
+            if (list.Contains(element)) return;
+            else list.Insert(index, element);
+        }
+
+        public bool Contains(T element)
+        {
+            return list.Contains(element);
+        }
+        public int IndexOf(T element) => list.IndexOf(element);
+
+        public void Remove(T element) => list.Remove(element);
+        public void RemoveAt(int index) => list.RemoveAt(index);
+
+        public void Clear() => list.Clear();
+
+        public T[] ToArray() => list.ToArray();
+
+        public void CopyTo(T[] array, int arrayIndex) => list.CopyTo(array, arrayIndex);
+
+        public void ForEach(Action<T> action) => list.ForEach(action);
+
+
+
+
+
+
     }
-    public void Insert(int index, T element)
-    {
-        if (list.Contains(element)) return;
-        else list.Insert(index, element);
-    }
-
-    public bool Contains(T element)
-    {
-        return list.Contains(element);
-    }
-    public int IndexOf(T element) => list.IndexOf(element);
-
-    public void Remove(T element) => list.Remove(element);
-    public void RemoveAt(int index) => list.RemoveAt(index);
-
-    public void Clear() => list.Clear();
-
-    public T[] ToArray() => list.ToArray();
-
-    public void CopyTo(T[] array, int arrayIndex) => list.CopyTo(array, arrayIndex);
-
-    public void ForEach(Action<T> action) => list.ForEach(action);
-
-
-
-
-
-
 }
