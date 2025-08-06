3b58e602d7c2931ea449995826406ffe62312618
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-08-06 03:08:05 -0700

    added unity events

---
+++ Scripts/Character/Player/PlayerInputs/UnityEvents.cs
@@ -0,0 +1,14 @@
+using DMBTools;
+using UnityEngine;
+
+public class UnityEvents : MonoBehaviour
+{
+    Character character;
+
+    void Awake()
+    {
+        character = GetComponent<Character>();
+    } 
+
+
+}
