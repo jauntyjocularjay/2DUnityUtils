a35008808d5d16b1ac80b25dcfa1fde6cad8fd3c
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-12-09 23:19:47 -0700

    established background and parallaxBackground

--- Scripts/Environments/Background/Background.cs
+++ Scripts/Environments/Background/Background.cs
@@ -1,35 +1,28 @@
 ﻿using UnityEngine;
 
+
+
 namespace DMBTools
 {
     public abstract class Background : DMBMonoBehaviour
     {
-        public Vector2 rateOfChange = Vector2.one;
-        Vector2 initialPosition;
-        Camera mainCamera;
-        Vector2 cameraInitialPosition;
-
+        protected Vector2 backgroundInitialPosition;
         new void Start()
         {
             base.Start();
-            initialPosition = new Vector2(Transform.position.x, Transform.position.y);
-            mainCamera = Camera.main;
-            cameraInitialPosition = mainCamera.transform.position;
+            backgroundInitialPosition = new Vector2
+            (
+                Transform.position.x,
+                Transform.position.y
+            );
         }
         void FixedUpdate()
         {
-            Vector2 totalChange = new Vector2
-            (
-                mainCamera.transform.position.x - cameraInitialPosition.x,
-                mainCamera.transform.position.y - cameraInitialPosition.y
-            );
             Transform.position = new Vector2
             (
-                initialPosition.x - totalChange.x * rateOfChange.x,
-                initialPosition.y - totalChange.y * rateOfChange.y
+                backgroundInitialPosition.x + Camera.main.transform.position.x,
+                backgroundInitialPosition.y + Camera.main.transform.position.y
             );
         }
-
     }
-
 }
---
+++ Scripts/Environments/Background/ParallaxBackground.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace DMBTools
+{
+    public abstract class ParallaxBackground : Background
+    {
+        public Vector2 rateOfChange = Vector2.zero;
+        Vector2 backgroundInitialLocalPosition;
+        Vector2 cameraInitialLocalPosition;
+
+        new void Start()
+        {
+            base.Start();
+            backgroundInitialLocalPosition = Transform.localPosition;
+            cameraInitialLocalPosition = Camera.main.transform.localPosition;
+        }
+        void FixedUpdate()
+        {
+            Scroll();
+        }
+
+        void Scroll()
+        {
+            Vector2 cameraPositionChange = new Vector2
+            (
+                Camera.main.transform.localPosition.x - cameraInitialLocalPosition.x,
+                Camera.main.transform.localPosition.y + cameraInitialLocalPosition.y
+            );
+            Vector2 backgroundLocalPosition = new Vector2
+            (
+                backgroundInitialLocalPosition.x + cameraPositionChange.x,
+                backgroundInitialLocalPosition.y + cameraPositionChange.y
+            );
+            // Camera Initial Position - The camera position is what the background positions will be based from
+            Transform.localPosition = new Vector2
+            (
+                backgroundLocalPosition.x - (backgroundLocalPosition.x * rateOfChange.x / 10),
+                backgroundLocalPosition.y - (backgroundLocalPosition.y * rateOfChange.y / 10)
+            );
+        }
+
+    }
+
+}
