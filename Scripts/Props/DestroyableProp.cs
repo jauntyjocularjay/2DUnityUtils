d9c31b2846f854e24388ab1a0ca5e2ad6eba6c53
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-12-18 03:55:51 -0700

    added destroyableprop, item, and itemgenerator

---
+++ Scripts/Props/DestroyableProp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+
+namespace DMBTools
+{
+
+    [RequireComponent(typeof(BoxCollider2D))]
+    public abstract class DestroyableProp : AnimatedProp
+    {
+        BoxCollider2D _BoxCollider2D;
+        new protected void Start()
+        {
+            base.Start();
+            _BoxCollider2D = GetComponent<BoxCollider2D>();
+        }
+        public BoxCollider2D BoxCollider2D
+        {
+            get => GetComponent<BoxCollider2D>();
+            set => BoxCollider2D = value;
+        }
+        void OnCollisionEnter2D(Collision2D collision) => HandleCollision(collision, TriggerType.Enter);
+        void OnCollisionStay2D(Collision2D collision) => HandleCollision(collision, TriggerType.Stay);
+        void OnCollisionExit2D(Collision2D collision) => HandleCollision(collision, TriggerType.Exit);
+        public abstract void HandleCollision(Collision2D collision, TriggerType triggerType);
+        void OnTriggerEnter2D(Collider2D collider) => HandleTrigger(collider, TriggerType.Enter);
+        void OnTriggerStay2D(Collider2D collider) => HandleTrigger(collider, TriggerType.Stay);
+        void OnTriggerExit2D(Collider2D collider) => HandleTrigger(collider, TriggerType.Exit);
+        public abstract void HandleTrigger(Collider2D collider, TriggerType triggerType);
+
+        public void SelfDestruct() => GameObject.Destroy(this);
+    }
+
+}
---
+++ Scripts/Props/ItemGenerator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+
+
+namespace DMBTools
+{
+    public class ItemGenerator : MonoBehaviour
+    {
+        public Item[] itemsToDrop;
+    }
+
+}
