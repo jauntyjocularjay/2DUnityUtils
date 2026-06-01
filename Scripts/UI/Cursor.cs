6a58376a15119730972379e0adee6224427b0d10
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2026-06-01 11:29:47 -0700

    added player input as required component for cursor

--- Scripts/UI/Cursor.cs
+++ Scripts/UI/Cursor.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace DMBTools
 {
+    [RequireComponent(typeof(PlayerInput))]    
     public abstract class Cursor : Prop
     {
         Vector2 position;
+        PlayerInput _playerInput;
+
+        PlayerInput PlayerInput
+        {
+            get => _playerInput;
+        }
 
         new void Start()
         {
             base.Start();
+
+            _playerInput = GetComponent<PlayerInput>();
+
             Transform.position = Vector2.zero;
         }
     }
