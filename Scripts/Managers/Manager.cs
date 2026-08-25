148fdb0f95b7736b5bfc20b62b5c407eebcdada7
Jay <77408854+jauntyjocularjay@users.noreply.github.com>
2026-08-25 16:19:13 -0700

    commented out unused fields

--- Scripts/Managers/Manager.cs
+++ Scripts/Managers/Manager.cs
@@ -9,9 +9,9 @@ namespace DMBTools
 {
     public abstract class Manager : DMBMonoBehaviour
     {
-        static readonly float mapUnitSide = 2.8125f;
+        // static readonly float mapUnitSide = 2.8125f;
         [Header("Camera Orthagonal Size Multiplier")]
-        static float size = 2.0f;
+        // static float size = 2.0f;
         public Scene nextScene;
         public Vector3 cameraMinimumPosition;
         public Vector3 cameraMaximumPosition;
