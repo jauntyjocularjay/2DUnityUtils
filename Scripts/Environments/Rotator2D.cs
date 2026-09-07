49d0d3bdf1a35199c8c7c96fd53cf140e889984d
Jay <77408854+jauntyjocularjay@users.noreply.github.com>
2026-09-06 17:36:18 -0700

    Created the Rotator2D script

---
+++ Scripts/Environments/Rotator2D.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Rotator2D : MonoBehaviour
+{
+    Quaternion endQuaternion;
+    public Vector3 endRotation;
+    public float rotationSpeed = 0.01f;
+    float timeCount = 0.0f;
+    public bool startRotation;
+    // The graphic needs to be on a child of the Rotator Game Object.
+
+    void Start()
+    {
+        endQuaternion = Quaternion.Euler(endRotation);
+    }
+
+    void Update()
+    {
+        if(startRotation)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, endQuaternion, timeCount * rotationSpeed);
+            timeCount += Time.deltaTime;
+            if (transform.rotation == endQuaternion)
+                startRotation = false;
+        }
+    }
+}
