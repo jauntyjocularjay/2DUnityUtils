c1fbf93648db7a579a5f61278969703baa0e7382
Jay <77408854+jauntyjocularjay@users.noreply.github.com>
2026-09-09 12:33:04 -0700

    ajustments to data,player data SOs

--- Scripts/Character/CharacterData.cs
+++ Scripts/Character/CharacterData.cs
@@ -7,8 +7,9 @@ namespace DMBTools
     {
         [Header("Character")]
         public int MaxHP = 0;
+        public int currentHP;
         public bool silhouette = false;
-        // public int ExtraJumps = 0;
+        public int jumps = 1;
         public Vector3 movementVelocity = Vector3.one;
     }
 
--- Scripts/Character/Player/PlayerData.cs
+++ Scripts/Character/Player/PlayerData.cs
@@ -1,20 +1,25 @@
-﻿using UnityEngine;
+﻿using UnityEditor.EditorTools;
+using UnityEngine;
 
 
 
 namespace DMBTools
 {
-    [CreateAssetMenu(fileName = "PlayerData", menuName = "DMBTools/Data/BoxPlayer", order = 0)]
-    public class BoxPlayerData : CharacterData
+    [CreateAssetMenu(fileName = "PlayerData", menuName = "DMBTools/Data/BoxPlayer", order = 99)]
+    public class PlayerData : CharacterData
     {
-        [Header("airControl is an integer between 0-12 that adds velocity during jumps.")]
+        public int maxHealth;
+        public int HP;
+        [Tooltip("airControl is an integer between 0-12 that adds velocity during jumps dictated by the input Vector.x")]
         public int airControl;
         public FractionScale airControlVelocity = new FractionScale(0, 12);
+
         public void AirControlVelocity(int i)
         {
 	        airControlVelocity.Numerator = i;
             airControl = i;
         }
+
         public float AirControlVelocity()
             => airControlVelocity.ToFloat();
     }
