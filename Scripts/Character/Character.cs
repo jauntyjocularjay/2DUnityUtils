3d45b7fad6577d4ea81f92981bf4c6932fae40c6
Jay <77408854+jauntyjocularjay@users.noreply.github.com>
2026-09-11 07:27:33 -0700

    Added audioSource to Character

--- Scripts/Character/Character.cs
+++ Scripts/Character/Character.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 namespace DMBTools
 {
+    [RequireComponent(typeof(SpriteRenderer))]
     [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(AudioSource))]
     public abstract class Character : MonoBehaviour
     /* @class Character extends Prop to provide everything needed to work with a character with a physical presence and animator effectively combining the AnimatedProp and PhysicalProp  */
     {
@@ -14,12 +16,18 @@ namespace DMBTools
         public Animator Animator { get => _Animator; }
         protected Rigidbody2D _Rigidbody2D;
         public Rigidbody2D Rigidbody2D { get => _Rigidbody2D; }
+        protected AudioSource _AudioSource;
+        public AudioSource AudioSource { get => _AudioSource; }
         public int HP = 0;
         protected void Start()
         {
+            _SpriteRenderer = GetComponent<SpriteRenderer>();
             _Animator = GetComponent<Animator>();
+
             _Rigidbody2D = GetComponent<Rigidbody2D>();
             _Rigidbody2D.freezeRotation = true;
+
+            _AudioSource = GetComponent<AudioSource>();
         }
 
         public void IncrementHP(int i = 1) => HP += i;
