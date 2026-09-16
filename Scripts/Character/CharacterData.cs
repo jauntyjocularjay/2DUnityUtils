dc4372e97697f6971c4700b5cc75aac529f12cac
Jay <77408854+jauntyjocularjay@users.noreply.github.com>
2026-09-15 22:46:05 -0700

    moved CharacterData health to PlayerData

--- Scripts/Character/CharacterData.cs
+++ Scripts/Character/CharacterData.cs
@@ -6,7 +6,6 @@ namespace DMBTools
     public abstract class CharacterData : ScriptableObject
     {
         [Header("Character")]
-        public int MaxHP = 0;
         public bool silhouette = false;
         public int jumps = 1;
         public Vector3 movementVelocity = Vector3.one;
--- Scripts/Character/Player/PlayerData.cs
+++ Scripts/Character/Player/PlayerData.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 
 namespace DMBTools
 {
-    [CreateAssetMenu(fileName = "PlayerData", menuName = "DMBTools/Data/BoxPlayer", order = 99)]
+    [CreateAssetMenu(fileName = "PlayerData", menuName = "DMBTools/Data/PlayerData", order = 99)]
     public class PlayerData : CharacterData
     {
         public int maxHealth;
-        public int HP;
         [Tooltip("airControl is an integer between 0-12 that adds velocity during jumps dictated by the input Vector.x")]
         public int airControl;
         public FractionScale airControlVelocity = new FractionScale(0, 12);
